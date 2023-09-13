8f2ace7315989dbddba2b35a94b0f8ce22c8f63e
Merlin 2100 <merlin2100nft@gmail.com>
2023-09-13 16:32:07 +0200

    UI setup - part 1

---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+
+        {/* Navbar */}
+
+        <main className='exchange grid'>
+          <section className='exchnage__section--left grid'>
+
+            {/* Markets */}
+            
+            {/* Balance */}
+
+            {/* Order */}
+
+          </section>
+          <section className='exchange__section--right grid'>
+
+            {/* PriceCahrt */}
+
+            {/* Transactions */}
+
+            {/* Trades */}
+
+            {/* OrderBook */}
+
+          </section>
+        </main>
+
+        {/* ALert */}
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
